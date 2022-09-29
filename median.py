0c8fca43c671963e8cbaed174d1ac0cc2938ee85
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2022-09-29 18:00:51 +0100

    Finish median task

--- median.py
+++ median.py
@@ -2,11 +2,38 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 while True:
+    
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        
     except ValueError:
         print("Some input could not be converted to a number!")
+        
     else:
         break
+    
 print(numbers)
+numbers.sort()
+
+def median():
+
+    if len(numbers) % 2 != 0:
+        median = numbers[int(len(numbers) / 2)]
+        print(median)
+        return median
+
+    else:
+        
+        length = len(numbers)
+        
+
+        a = numbers[int(length / 2)]
+        b = numbers[int((length / 2) - 1)]
+
+        median = (a + b) / 2
+        print(median)
+        return median
+
+
+median()
